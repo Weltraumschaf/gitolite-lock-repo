c379d9257a7ab9b7c774cadf4929f1b72699e774
Sven Strittmatter <Sven.Strittmatter@icongmbh.de>
2014-09-25 18:14:16 +0200

    Add own lib.

---
+++ src/lib/Gitolite/LockRepo.pm
@@ -0,0 +1,12 @@
+package Gitolite::LockRepo;
+
+# Common functions for lock-repo command and update hook.
+# -------------------------------------------------------
+
+#<<<
+@EXPORT = qw(
+);
+#>>>
+use Exporter 'import';
+
+1;
